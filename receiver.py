1a02ea0acc4e84fb2e736de8ba9e5169475a93ad
wituwitu <diego.wistuba@ug.uchile.cl>
2019-11-14 22:31:34 -0300

    Funciona con pérdidas de hasta 99%. Documenta3 el receiver y el sender.

--- receiver.py
+++ receiver.py
@@ -21,7 +21,8 @@ def find(string, character):
 	return [i for i, ltr in enumerate(string) if ltr == character]
 
 
-def get_fragments(device_id, limit, auth="Basic NWRjNDY1ZjRlODMzZDk0MWY2Y2M0Y2QzOjBmMjQ1NDI0MTg2YTMxNDhjYzJkNWJiYjI0MDUwOWY5"):
+def get_fragments(device_id, limit,
+				  auth="Basic NWRjNDY1ZjRlODMzZDk0MWY2Y2M0Y2QzOjBmMjQ1NDI0MTg2YTMxNDhjYzJkNWJiYjI0MDUwOWY5"):
 	url = "https://api.sigfox.com/v2/devices/{}/messages".format(device_id)
 	payload = ''
 	headers = {
@@ -39,194 +40,180 @@ def get_fragments(device_id, limit, auth="Basic NWRjNDY1ZjRlODMzZDk0MWY2Y2M0Y2Qz
 		byte_array.append(bytearray.fromhex(val["data"]))
 
 
+# Delete the previously received file (only on offline testing)
 for filename in glob.glob("received*"):
 	os.remove(filename)
 
 print("This is the RECEIVER script for a Sigfox Uplink transmission example")
 
-profile_uplink = Sigfox("UPLINK", "ACK ON ERROR")
-profile_downlink = Sigfox("DOWNLINK", "NO ACK")
-
-buffer_size = profile_uplink.MTU
-the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 if len(sys.argv) != 3:
 	print("python receiver.py [PORT] [UPLINK LOSS RATE]")
+	sys.exit()
 
 ip = ""
 port = int(sys.argv[1])
 loss_rate = int(sys.argv[2])
 
-the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-the_socket.bind((ip, port))
-
-current_size = 0
-
+# Initialize variables.
+profile_uplink = Sigfox("UPLINK", "ACK ON ERROR")
+profile_downlink = Sigfox("DOWNLINK", "NO ACK")
+buffer_size = profile_uplink.MTU
 n = profile_uplink.N
 w = profile_uplink.WINDOW_SIZE
-
+the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+the_socket.bind((ip, port))
+current_window = 0
 fragments = []
-
-print(len(fragments))
-
-ack_list = []
+window = []
+payload = ''
 bitmap = ''
-
 for l in range(profile_uplink.BITMAP_SIZE):
 	bitmap += '0'
-
-i = 0
-payload = ''
-
-
-# w = bin(int(floor((i/(2**n - 1) % (2 ** window_size)))))[2:].zfill(2)
-# fcn = bin((2 ** n - 2) - (i % (2 ** n - 1)))[2:].zfill(3)
-n = profile_uplink.N
 fcn_dict = {}
-
 for j in range(2 ** n - 1):
-	# print(j)
 	fcn_dict[bin((2 ** n - 2) - (j % (2 ** n - 1)))[2:].zfill(3)] = j
 
-current_window = 0
-window = []
-
+# Start receiving fragments.
 while True:
-	# print("Expecting " + str(i + 1) + "th fragment.")
-	# fcn = bin((2 ** n - 2) - (i % (2 ** n - 1)))[2:].zfill(3)
 
+	# Initialize window
 	for i in range(2 ** n - 1):
 		window.append(b"")
 
+	# Set the timeout for INACTIVITY_TIMER_VALUE
 	the_socket.settimeout(profile_uplink.INACTIVITY_TIMER_VALUE)
 
-	fragment, address = the_socket.recvfrom(buffer_size)
+	# Receive fragment
+	try:
+		fragment, address = the_socket.recvfrom(buffer_size)
 
-	if fragment:
+		# A fragment has the format "fragment = [header, payload]".
 		data = [bytes([fragment[0]]), bytearray(fragment[1:])]
 
-		try:
-			if fragment.decode() == "":
-				break
-		except UnicodeDecodeError:
-			pass
-
+		# Convert to a Fragment class for easier manipulation.
 		fragment_message = Fragment(profile_uplink, data)
 
-		# print("Received FCN: " + fragment_message.header.FCN)
-
-		# This avoids All-0 and All-1 fragments being lost. A bit unreal, if you ask me...
-
+		# This avoids All-0 and All-1 fragments being lost for testing purposes.
+		# TODO: What happens when All-0 or All-1 get lost?
 		if not fragment_message.is_all_0() and not fragment_message.is_all_1():
+
+			# Lose packet with certain probability and go to the start of the loop.
 			coin = random.random()
 			if coin * 100 < loss_rate:
 				print("[LOSS] The packet was lost.")
-				i += 1
 				continue
 
+		# Try finding the fragment number from the FCN of the fragment.
 		try:
 			fragment_number = fcn_dict[fragment_message.header.FCN]
 			print("[RECV] This corresponds to the " + str(fragment_number) + "th fragment of the " + str(
 				current_window) + "th window.")
+
+			# Set the fragment_number-th bit of the bitmap to 1 and add the fragment to the fragment_number-th position
+			# in the current window.
 			bitmap = replace_bit(bitmap, fragment_number, '1')
-			# print(bitmap)
-			# add the received fragment to the array
 			window[fragment_number] = data
 
+		# If the FCN does not have a corresponding fragment number, then it almost certainly is an All-1
 		except KeyError:
+
+			# Set the rightmost bit of the bitmap to 1 (See SCHC draft).
 			print("[RECV] This seems to be the final fragment.")
-			fragment_number = (2**n - 1) - 1
+			bitmap = replace_bit(bitmap, len(bitmap) - 1, '1')
 
-			# If everything has gone according to plan, there shouldn't be any empty fragments between two fragments
-			# So the first occurrence of an empty fragment should be the position of the final fragment.
-			# Do I make myself clear?? D:
-
-			window[window.index(b"")] = data
-
+		# Extract information from the fragment class.
 		rule_id = fragment_message.header.RULE_ID
 		dtag = fragment_message.header.DTAG
 		w = fragment_message.header.W
 
-		# add the received fragment to the bitmap
-
-		current_size += len(fragment)
-		# print("[RECV] Received " + str(current_size) + " so far...")
-
+		# If the fragment is at the end of a window (All-0 or All-1).
 		if fragment_message.is_all_0() or fragment_message.is_all_1():
 
-			print("[ALLX] Received All-X: Sending ACK if it exists...")
-			print("[ALLX] Bitmap: " + bitmap)
-
+			# Check for lost fragments in the bitmap.
 			if '0' in bitmap:
 
 				number_of_lost_fragments = bitmap.count('0')
 				indices = find(bitmap, '0')
 
+				# Create an ACK object and send it as bytes to the sender.
 				print("[ALLX] Sending NACK for lost fragments...")
 				ack = ACK(profile_downlink, rule_id, dtag, w, bitmap, '0')
 				the_socket.sendto(ack.to_bytes(), address)
 
+				# For every lost fragment:
 				for j in range(number_of_lost_fragments):
 					index = indices[j]
 
-					print("[ALLX] Recovering " + str(index) + "th fragment...")
-					fragment, address = the_socket.recvfrom(buffer_size)
+					# Try recovering the index-th fragment
+					try:
+						print("[ALLX] Recovering " + str(index) + "th fragment...")
+						fragment_recovered, address = the_socket.recvfrom(buffer_size)
+						data_recovered = [bytes([fragment_recovered[0]]), bytearray(fragment_recovered[1:])]
+						fragment_message_recovered = Fragment(profile_uplink, data_recovered)
+						fragment_number_recovered = fcn_dict[fragment_message_recovered.header.FCN]
+						print("[ALLX] This corresponds to the " + str(
+							fragment_number_recovered) + "th fragment of the " + str(
+							current_window) + "th window.")
+						window[index] = data_recovered
+						print("[ALLX] Recovered")
 
-					data = [bytes([fragment[0]]), bytearray(fragment[1:])]
+					# If the index-th fragment's FCN does not have a fragment number, then it is invalid
+					except KeyError:
+						print("No fragment to be recovered")
+						continue
 
-					fragment_message = Fragment(profile_uplink, data)
+					# If no fragment was received, something nasty happened.
+					except socket.timeout:
+						print("Timed out")
+						exit(1)
 
-					fragment_number = fcn_dict[fragment_message.header.FCN]
-
-					print("[ALLX] This corresponds to the " + str(fragment_number) + "th fragment of the " + str(
-						current_window) + "th window.")
-
-					window[index] = data
-					current_size += len(fragment)
-
-					print("[ALLX] Recovered")
-			# print("[ALL0] Received " + str(current_size) + " so far...")
-
+			# Add all fragments from the window to the "fragments" array
 			for m in range(2 ** n - 1):
 				fragments.append(window[m])
 
+			# If the last received fragment is an All-0, reinitialize variables for next loop
 			if fragment_message.is_all_0():
-
-				# reinitialize
 				current_window += 1
 				window = []
 				bitmap = ''
 				for k in range(profile_uplink.BITMAP_SIZE):
 					bitmap += '0'
 
+			# If the last received fragment is an All-1, start reassembling.
 			if fragment_message.is_all_1():
 
-				#print(fragments)
+				# If everything has gone according to plan, there shouldn't be any empty spaces
+				# between two received fragments. So the first occurrence of an empty space should be the position
+				# of the final fragment.
+				last_index = fragments.index(b'')
+				fragments[last_index] = data
 
+				# Reassemble.
 				print("[ALL1] Last fragment. Reassembling...")
-
-				print("[ALL1] " + str(len(fragments)) + " fragments received.")
-
 				reassembler = Reassembler(profile_uplink, fragments)
-
-				# try:
 				payload = bytearray(reassembler.reassemble())
+
+				# Send the last ACK with the C bit set to 1.
 				print("[ALL1] Reassembled: Sending last ACK")
+
+				# "if the C bit is set to 1, no bitmap is carried."
+				bitmap = ''
+				for k in range(profile_uplink.BITMAP_SIZE):
+					bitmap += '0'
+
 				last_ack = ACK(profile_downlink, rule_id, dtag, w, bitmap, '1')
 				the_socket.sendto(last_ack.to_bytes(), address)
 
-				# except:
-				# 	print("Could not reassemble ):")
-
+				# End loop
 				break
 
-	i += 1
+	# If no fragment was received, something nasty happened.
+	except socket.timeout:
+		print("Timed out")
+		exit(1)
 
+# Close the socket and write the received file.
 the_socket.close()
-
-# print(payload)
-
 file = open("received.png", "wb")
 file.write(payload)
 file.close()
--- sender.py
+++ sender.py
@@ -2,20 +2,14 @@
 
 import socket
 import sys
-import os
 import time
+import filecmp
 
 from Entities.Fragmenter import Fragmenter
 from Entities.Sigfox import Sigfox
-from Messages.ACK import ACK
 from Messages.Fragment import Fragment
 
 print("This is the SENDER script for a Sigfox Uplink transmission example")
-
-profile_uplink = Sigfox("UPLINK", "ACK ON ERROR")
-profile_downlink = Sigfox("DOWNLINK", "NO ACK")
-
-the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if len(sys.argv) != 4:
 	print("python sender.py [IP] [PORT] [FILENAME]")
@@ -24,135 +18,146 @@ if len(sys.argv) != 4:
 ip = sys.argv[1]
 port = int(sys.argv[2])
 filename = sys.argv[3]
-
 address = (ip, port)
 
-# data = open(filename, "rb")
-# payload = data.read().decode()
-# data.close()
-
+# Read the file to be sent.
 with open(filename, "rb") as data:
 	f = data.read()
 	payload = bytearray(f)
 
-# print(f)
-
-# print(payload)
+# Initialize variables.
 total_size = len(payload)
 current_size = 0
 percent = round(0, 2)
-
-fragmenter = Fragmenter(profile_uplink, payload)
-fragment_list = fragmenter.fragment()
-
 ack = None
 last_ack = None
 i = 0
-
 current_window = 0
-# window = fragment_list[:profile_uplink.WINDOW_SIZE]
+profile_uplink = Sigfox("UPLINK", "ACK ON ERROR")
+profile_downlink = Sigfox("DOWNLINK", "NO ACK")
+the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Fragment the file.
+fragmenter = Fragmenter(profile_uplink, payload)
+fragment_list = fragmenter.fragment()
+
+# Start sending fragments.
 while i < len(fragment_list):
-	# try:
-	#     data = fragment_list[i]
-	# except IndexError:
-	#     print("Sending empty message")
-	#     the_socket.sendto("".encode(), address)
-	#     break
 
+	# A fragment has the format "fragment = [header, payload]".
 	data = bytes(fragment_list[i][0] + fragment_list[i][1])
-
-	# print(profile_uplink.MTU)
-	# print(len(data)*8)
-	# print(len(bytes(fragment_list[i][0])))
-	# print(len(bytes(fragment_list[i][1])))
-
-	the_socket.sendto(data, address)
-
-	current_size += len(data)
+	current_size += len(fragment_list[i][1])
 	percent = round(float(current_size) / float(total_size) * 100, 2)
 
+	# Send the data.
 	print("Sending...")
+	the_socket.sendto(data, address)
 	print(str(current_size) + " / " + str(total_size) + ", " + str(percent) + "%")
 
-	# print("converting to fragment")
-
+	# Convert to a Fragment class for easier manipulation.
 	fragment = Fragment(profile_uplink, fragment_list[i])
 
-	print("Current window: " + str(fragment.header.W))
+	# If a fragment is an All-0 or an All-1:
+	if fragment.is_all_0() or fragment.is_all_1():
 
-	if fragment.is_all_0():
-		the_socket.settimeout(1)  # profile_uplink.RETRANSMISSION_TIMER_VALUE
+		# Set the timeout for RETRANSMISSION_TIMER_VALUE.
+		the_socket.settimeout(profile_uplink.RETRANSMISSION_TIMER_VALUE)
+
+		# Try receiving an ACK from the receiver.
 		try:
 			ack, address = the_socket.recvfrom(profile_downlink.MTU)
-			print("Oh no! ACK received.")
-			print(ack.decode())
+			print("ACK received.")
 			index = profile_uplink.RULE_ID_SIZE + profile_uplink.T + profile_uplink.WINDOW_SIZE
 			bitmap = ack.decode()[index:index + profile_uplink.BITMAP_SIZE]
-			print(bitmap)
+			index_c = index + profile_uplink.BITMAP_SIZE
+			c = ack.decode()[index_c]
 
-			# unsent_fragments = []
-
-			for j in range(len(bitmap)):
-				if bitmap[j] == '0':
-					print("The " + str(j) + "th (" + str((2 ** profile_uplink.N - 1)*current_window + j) + ") fragment was lost! Sending again...")
-
-					fragment_to_be_resent = fragment_list[(2 ** profile_uplink.N - 1)*current_window + j]
-
-					data = bytes(fragment_to_be_resent[0] + fragment_to_be_resent[1])
-					print(data)
-					the_socket.sendto(data, address)
-
-			print("Proceeding to next window")
-			current_window += 1
-		except socket.timeout:
-			print("No ACK received.")
-			print("Proceeding to next window")
-			current_window += 1
-
-	# En este caso, se tiene la opción de enviar al final un SCHC ACK REQ para verificar que los
-	# fragmentos retransmitidos han sido recibidos correctamente, o no. Esto es algo que aún no me queda
-	# claro si sea estrictamente necesario o si podemos simplemente continuar con la transmisión de los
-	# siguientes fragmentos… Tiendo a esta última opción.
-
-	if fragment.is_all_1():
-		print("Waiting for last ACK...")
-		requests = 1
-		while requests <= profile_uplink.MAX_ACK_REQUESTS:
-			the_socket.settimeout(profile_uplink.RETRANSMISSION_TIMER_VALUE)
-			try:
-				last_ack, address = the_socket.recvfrom(profile_downlink.MTU)
-				index = profile_uplink.RULE_ID_SIZE + profile_uplink.T + 2*profile_uplink.WINDOW_SIZE
-				c = last_ack.decode()[index]
-				print("Last ACK received with C = " + c + ". End of transmission.")
-				the_socket.sendto("".encode(), address)
+			# If the C bit of the ACK is set to 1 and the fragment is an All-1 then we're done.
+			if c == '1' and fragment.is_all_1():
+				# TODO: Check if the W field is correct (see SCHC draft)
+				print("Last ACK received: Fragments reassembled successfully. End of transmission.")
 				break
-			except:
-				requests += 1
-				print("Trying for " + str(requests) + "th time")
-			if requests > profile_uplink.MAX_ACK_REQUESTS:
-				print("MAX_ACK_REQUESTS reached. Wat do?")
 
-	# for ack in ack_list:  # para cada ACK recibido
-	# 	fcn = ACK(profile_downlink, ack).header.FCN
-	# 	# obtengo su FCN # OJO AQUI: ACK crea un ACK para el ACK, no lo reinstaura.
-	# 	# Pero los headers son iwales así que igual apaña
-	# 	for fragment in fragment_list:
-	# 		if fcn == Fragment(profile_uplink, fragment).header.FCN:
-	# 			the_socket.sendto(fragment.encode(), address)
-	# 			# reenvío todos los fragmentos cuyos FCN aparezcan en ACKs
-	# 		else:
-	# 			print("Received ACK but no corresponding fragment found D:")
+			# If the C bit is set to 1 and the fragment is an All-0 then something naughty happened.
+			elif c == '1' and fragment.is_all_0():
+				print("You shouldn't be here. (All-0 with C = 1)")
+				exit(1)
 
-	if last_ack:
-		break
-	else:
-		i += 1
+			# If the C bit has not been set:
+			elif c == '0':
 
+				# Check the bitmap.
+				for j in range(len(bitmap)):
+
+					# If the j-th bit of the bitmap is 0, then the j-th fragment was lost.
+					if bitmap[j] == '0':
+						print("The " + str(j) + "th (" + str(
+							(2 ** profile_uplink.N - 1) * current_window + j) + " / " + str(
+							len(fragment_list)) + ") fragment was lost! Sending again...")
+
+						# Try sending again the lost fragment.
+						try:
+							fragment_to_be_resent = fragment_list[(2 ** profile_uplink.N - 1) * current_window + j]
+							data_to_be_resent = bytes(fragment_to_be_resent[0] + fragment_to_be_resent[1])
+							print(data_to_be_resent)
+							the_socket.sendto(data_to_be_resent, address)
+						# TODO: What happens when the resent fragment gets lost?
+
+						# If the fragment wasn't found, it means we're at the last window with no fragment
+						# to be resent. The last fragment received is an All-1.
+						except IndexError:
+							print("No fragment found.")
+
+							# Request last ACK sending the All-1 again.
+							the_socket.sendto(data, address)
+
+				# After sending the lost fragments, if the last received fragment was an All-1 we need to receive
+				# the last ACK.
+				if fragment.is_all_1():
+
+					# Set the timeout for RETRANSMISSION_TIMER_VALUE
+					the_socket.settimeout(profile_uplink.RETRANSMISSION_TIMER_VALUE)
+
+					# Try receiving the last ACK.
+					try:
+						last_ack, address = the_socket.recvfrom(profile_downlink.MTU)
+						c = last_ack.decode()[index_c]
+
+						# If the C bit is set to 1 then we're done.
+						if c == '1':
+							print("Last ACK received: Fragments reassembled successfully. End of transmission.")
+							break
+
+						# If the C bit is 0 then something naughty happened.
+						else:
+							print("You shouldn't be here. (Last ACK with C = 0)")
+							exit(0)
+
+					# If the last ACK was not received, raise an error.
+					except socket.timeout:
+						# TODO: Add requests up to MAX_ACK_REQUESTS
+						print("Last ACK has not been received.")
+						exit(1)
+
+			# Proceed to next window.
+			print("Proceeding to next window")
+			current_window += 1
+
+		# If no ACK was received, it's safe to assume that no fragments went lost... Or did they?
+		except socket.timeout:
+			# TODO: What happens when the ACK gets lost?
+			print("No ACK received. We'll assume that no fragments were lost :D")
+
+			# Proceed to next window.
+			print("Proceeding to next window")
+			current_window += 1
+
+	# Continue to next fragment
+	i += 1
+
+# Close the socket and wait for the file to be reassembled
 the_socket.close()
-
 time.sleep(1)
 
-import filecmp
-
+# Compare if the reassembled file is the same as the original (only on offline testing)
 print(filecmp.cmp("received.png", filename))
