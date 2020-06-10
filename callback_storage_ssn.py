b9456c0800142802bbe3acdda6a9197867615aae
wituwitu <diego.wistuba@ug.uchile.cl>
2020-06-10 16:43:28 -0400

    Se agrega la lógica de los Sigfox Sequence Numbers (SSN)

--- callback_storage_ssn.py
+++ callback_storage_ssn.py
@@ -355,14 +355,8 @@ def callback_data(request):
 					return ("0" * (width - len(string))) + string
 				else:
 					return string
-			def insert_index(ls, pos, elmt):
-				while len(ls) < pos:
-					ls.append([])
-				ls.insert(pos, elmt)
 			def replace_bit(string, position, value):
 				return '%s%s%s' % (string[:position], value, string[position + 1:])
-			def find(string, character):
-				return [i for i, ltr in enumerate(string) if ltr == character]
 
 			fragment = request_dict["data"]
 			sigfox_sequence_number = request_dict["seqNumber"]
